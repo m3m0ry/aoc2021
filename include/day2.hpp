1303a24868c7fc23f14d2f264b4f586cb9fa227f
Jan Hönig <jan.hoenig@fau.de>
2021-12-02 06:34:02 +0100

    Finished day2

--- include/day2.hpp
+++ include/day2.hpp
@@ -2,10 +2,16 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <utility>
+#include <unordered_map>
+
+enum dir { forward, down, up };
+
+static std::unordered_map<std::string,dir> const table = {{"forward", dir::forward}, {"down",dir::down}, {"up", dir::up}};
 
 struct Day2{
     void parse(std::vector<std::string> input);
     std::string part1() const;
     std::string part2() const;
-    std::vector<int> numbers;
+    std::vector<std::pair<dir, int>> movement;
 };
--- src/day2.cpp
+++ src/day2.cpp
@@ -12,15 +12,53 @@
 namespace rv = std::ranges::views;
 namespace ranges = std::ranges;
 
+using namespace std::placeholders;
+
 void Day2::parse(std::vector<std::string> input) {
-    auto view = input | rv::transform(strutil::parse_string<int>);
-    ranges::copy(view, std::back_inserter(numbers));
+    auto view = input | rv::transform([](auto l)
+            {auto vec = strutil::split(l, " ");
+             return std::make_pair(table.at(vec[0]), strutil::parse_string<int>(vec[1]));});
+    ranges::copy(view, std::back_inserter(movement));
 }
 
 std::string Day2::part1() const {
-    return "Not Implemented!!!";
+    int horizontal = 0;
+    int depth = 0;
+    auto sc = [&horizontal, &depth](auto p){
+        switch(p.first) {
+            case dir::forward:
+                horizontal += p.second;
+                break;
+            case dir::up:
+                depth -= p.second;
+                break;
+            case dir::down:
+                depth += p.second;
+                break;
+        }
+    };
+    ranges::for_each(movement, sc);
+    return strutil::to_string(horizontal * depth);
 }
 
 std::string Day2::part2() const {
-    return "Not Implemented!!!";
+    int horizontal = 0;
+    int depth = 0;
+    int aim = 0;
+    auto sc = [&horizontal, &depth, &aim](auto p){
+        switch(p.first) {
+            case dir::forward:
+                horizontal += p.second;
+                depth += aim * p.second;
+                break;
+            case dir::up:
+                aim -= p.second;
+                break;
+            case dir::down:
+                aim += p.second;
+                break;
+        }
+    };
+    ranges::for_each(movement, sc);
+    return strutil::to_string(horizontal * depth);
 }
